e01fa5afba78c1165b61ee4ffe63fc2e6e48ce5f
Itay Newman <itaynewman@gmail.com>
2025-05-05 19:23:28 +0300

    nigga, added basic function in MenuRequestHandler

--- Trivia Server/MenuRequestHandler.cpp
+++ Trivia Server/MenuRequestHandler.cpp
@@ -3,3 +3,28 @@
 MenuRequestHandler::MenuRequestHandler(IDatabase& database, LoginManager* loginManager) : m_database(database), m_loginManager(loginManager)
 {
 }
+
+bool MenuRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
+{
+	return false;
+}
+
+RequestResult MenuRequestHandler::handleRequest(const RequestInfo& requestInfo)
+{
+	return RequestResult();
+}
+
+bool MenuRequestHandler::doesUserExist(const std::string& username) const
+{
+	return false;
+}
+
+bool MenuRequestHandler::doesPasswordMatch(const std::string& username, const std::string& password) const
+{
+	return false;
+}
+
+bool MenuRequestHandler::addUser(const std::string& username, const std::string& password, const std::string& email) const
+{
+	return false;
+}
