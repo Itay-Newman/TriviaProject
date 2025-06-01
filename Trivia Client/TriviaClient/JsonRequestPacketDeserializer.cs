1b1c3c62202b03e8e94450de8525083658f2a873
Eitan Saidian <eitan4810@gmail.com>
2025-06-01 13:57:13 +0300

    finishing the issues

--- Trivia Client/TriviaClient/JsonRequestPacketDeserializer.cs
+++ Trivia Client/TriviaClient/JsonRequestPacketDeserializer.cs
@@ -11,23 +11,35 @@ namespace TriviaClient
             return JsonConvert.DeserializeObject<T>(json);
         }
 
-        public static LoginResponse DeserializeLoginResponse(byte[] buffer) =>
-            Deserialize<LoginResponse>(buffer);
+        public static LoginResponse DeserializeLoginResponse(byte[] buffer)
+        {
+            return Deserialize<LoginResponse>(buffer);
+        }
 
-        public static SignupResponse DeserializeSignupResponse(byte[] buffer) =>
-            Deserialize<SignupResponse>(buffer);
+        public static SignupResponse DeserializeSignupResponse(byte[] buffer)
+        {
+            return Deserialize<SignupResponse>(buffer);
+        }
 
-        public static LogoutResponse DeserializeLogoutResponse(byte[] buffer) =>
-            Deserialize<LogoutResponse>(buffer);
+        public static LogoutResponse DeserializeLogoutResponse(byte[] buffer)
+        {
+            return Deserialize<LogoutResponse>(buffer);
+        }
 
-        public static GetRoomsResponse DeserializeGetRoomsResponse(byte[] buffer) =>
-            Deserialize<GetRoomsResponse>(buffer);
+        public static GetRoomsResponse DeserializeGetRoomsResponse(byte[] buffer)
+        {
+            return Deserialize<GetRoomsResponse>(buffer);
+        }
 
-        public static GetPlayersInRoomResponse DeserializeGetPlayersInRoomResponse(byte[] buffer) =>
-            Deserialize<GetPlayersInRoomResponse>(buffer);
+        public static GetPlayersInRoomResponse DeserializeGetPlayersInRoomResponse(byte[] buffer)
+        {
+            return Deserialize<GetPlayersInRoomResponse>(buffer);
+        }
 
-        public static JoinRoomResponse DeserializeJoinRoomResponse(byte[] buffer) =>
-            Deserialize<JoinRoomResponse>(buffer);
+        public static JoinRoomResponse DeserializeJoinRoomResponse(byte[] buffer)
+        {
+            return Deserialize<JoinRoomResponse>(buffer);
+        }
 
         public static CreateRoomResponse DeserializeCreateRoomResponse(byte[] buffer)
         {
@@ -37,37 +49,59 @@ namespace TriviaClient
             return response;
         }
 
-        public static GetHighScoreResponse DeserializeGetHighScoreResponse(byte[] buffer) =>
-            Deserialize<GetHighScoreResponse>(buffer);
+        public static GetHighScoreResponse DeserializeGetHighScoreResponse(byte[] buffer)
+        {
+            return Deserialize<GetHighScoreResponse>(buffer);
+        }
 
-        public static GetPersonalStatsResponse DeserializeGetPersonalStatsResponse(byte[] buffer) =>
-            Deserialize<GetPersonalStatsResponse>(buffer);
+        public static GetPersonalStatsResponse DeserializeGetPersonalStatsResponse(byte[] buffer)
+        {
+            return Deserialize<GetPersonalStatsResponse>(buffer);
+        }
 
-        public static CloseRoomResponse DeserializeCloseRoomResponse(byte[] buffer) =>
-            Deserialize<CloseRoomResponse>(buffer);
+        public static CloseRoomResponse DeserializeCloseRoomResponse(byte[] buffer)
+        {
+            return Deserialize<CloseRoomResponse>(buffer);
+        }
 
-        public static StartGameResponse DeserializeStartGameResponse(byte[] buffer) =>
-            Deserialize<StartGameResponse>(buffer);
+        public static StartGameResponse DeserializeStartGameResponse(byte[] buffer)
+        {
+            return Deserialize<StartGameResponse>(buffer);
+        }
 
-        public static GetRoomStateResponse DeserializeGetRoomStateResponse(byte[] buffer) =>
-            Deserialize<GetRoomStateResponse>(buffer);
+        public static GetRoomStateResponse DeserializeGetRoomStateResponse(byte[] buffer)
+        {
+            return Deserialize<GetRoomStateResponse>(buffer);
+        }
 
-        public static LeaveRoomResponse DeserializeLeaveRoomResponse(byte[] buffer) =>
-            Deserialize<LeaveRoomResponse>(buffer);
+        public static LeaveRoomResponse DeserializeLeaveRoomResponse(byte[] buffer)
+        {
+            return Deserialize<LeaveRoomResponse>(buffer);
+        }
 
-        public static LeaveGameResponse DeserializeLeaveGameResponse(byte[] buffer) =>
-            Deserialize<LeaveGameResponse>(buffer);
+        public static LeaveGameResponse DeserializeLeaveGameResponse(byte[] buffer)
+        {
+            return Deserialize<LeaveGameResponse>(buffer);
+        }
 
-        public static GetQuestionResponse DeserializeGetQuestionResponse(byte[] buffer) =>
-            Deserialize<GetQuestionResponse>(buffer);
+        public static GetQuestionResponse DeserializeGetQuestionResponse(byte[] buffer)
+        {
+            return Deserialize<GetQuestionResponse>(buffer);
+        }
 
-        public static SubmitAnswerResponse DeserializeSubmitAnswerResponse(byte[] buffer) =>
-            Deserialize<SubmitAnswerResponse>(buffer);
+        public static SubmitAnswerResponse DeserializeSubmitAnswerResponse(byte[] buffer)
+        {
+            return Deserialize<SubmitAnswerResponse>(buffer);
+        }
 
-        public static GetGameResultsResponse DeserializeGetGameResultsResponse(byte[] buffer) =>
-            Deserialize<GetGameResultsResponse>(buffer);
+        public static GetGameResultsResponse DeserializeGetGameResultsResponse(byte[] buffer)
+        {
+            return Deserialize<GetGameResultsResponse>(buffer);
+        }
 
-        public static ErrorResponse DeserializeErrorResponse(byte[] buffer) =>
-            Deserialize<ErrorResponse>(buffer);
+        public static ErrorResponse DeserializeErrorResponse(byte[] buffer)
+        {
+            return Deserialize<ErrorResponse>(buffer);
+        }
     }
 }
